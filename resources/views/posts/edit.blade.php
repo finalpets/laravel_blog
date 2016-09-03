57bd2df94a3363dbce680e856f4b65251092fa10
Peter Leyva <pets.leyva.baza@gmail.com>
2016-09-03 00:12:42 -0700

    FInished Video 44 Output HTML from Database

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,6 +4,18 @@
 
 @section('stylesheet')	
 	{!! Html::style('css/select2.min.css') !!}
+
+	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+
+	<script>
+		tinymce.init({
+			selector: 'textarea',
+			plugins: 'link code',
+			menubar: false
+
+		});
+		
+	</script>
 @endsection
 
 @section('content')
